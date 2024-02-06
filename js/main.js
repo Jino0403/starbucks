45d61a0a242af2c98d980db7d929f8341f42808b
Jino0403 <jinho9203@naver.com>
2024-02-06 23:33:37 +0900

    feat: 반복 애니메이션 기능 추가

--- js/main.js
+++ js/main.js
@@ -100,3 +100,34 @@ promotionToggleBtn.addEventListener('click', () => {
       promotionEl.classList.remove('hide'),
     ]
 })
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소,시간,옵션)
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), //애니메이션 동작 시간
+    {
+      // 옵션
+      y: size,
+      //y축으로 20만큼 내려오는데
+      repeat: -1,
+      // 반복여부를 -1로 설정하면 무한반복
+      yoyo: true,
+      // 한번 재생된 애니메이션 다시 뒤로 재생
+      ease: 'power1.inOut',
+      // 반복관련 gsap easing 함수 검색해서 참조, 반복을 자연스럽게 하기위한 추가
+      delay: random(0, delay),
+    },
+  )
+}
+
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', 0.5, 15)
+floatingObject('.floating3', 1.5, 20)
